89b1d129e5c5dabb23cce4a4b29e42ba2eb045e0
Facundo Gauna <facundo.gauna@newsignature.com>
2020-02-07 23:38:21 -0500

    added variable blocks

--- iac/service-principal.tf
+++ iac/service-principal.tf
@@ -1,6 +1,26 @@
+variable "subscription_id" {  
+}
+
+variable "client_id" {  
+}
+
+variable "client_secret" {
+
+}
+
+variable "tenant_id" {
+  
+}
+
+
 # Configure the Microsoft Azure Active Directory Provider
 provider "azuread" {
 #   version = "=0.3.0"
+
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
 }
 
 # Create an application
