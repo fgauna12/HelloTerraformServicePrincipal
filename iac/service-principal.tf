961852500fd6121fcd5fcfb7978b40db0527fb0e
Facundo Gauna <facundo.gauna@newsignature.com>
2020-02-07 23:22:14 -0500

    Creating service principal

---
+++ iac/service-principal.tf
@@ -0,0 +1,14 @@
+# Configure the Microsoft Azure Active Directory Provider
+provider "azuread" {
+  version = "=0.3.0"
+}
+
+# Create an application
+resource "azuread_application" "aplication_registration" {
+  name = "HelloTerraform"
+}
+
+# Create a service principal
+resource "azuread_service_principal" "service_principal" {
+  application_id = "${azuread_application.aplication_registration.application_id}"
+}
